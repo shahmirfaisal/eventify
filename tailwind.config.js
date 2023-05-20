6395ba8c354ba165bab856d18dad9829e9462275
Shahmir Faisal <shahmir049@gmail.com>
2023-05-21 00:24:09 +0500

    initialized with nextjs, tailwind, daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
   ],
+
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-poppins)"]
       },
-    },
+
+      colors: {
+        primary: "#49111C",
+        secondary: "#F2F4F3"
+      },
+
+      container: {
+        center: true
+      }
+    }
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#49111C",
+          secondary: "#F2F4F3"
+        }
+      }
+    ]
+  }
 }
